714467c741e9c5d1b1dff20b0ee4ed9d8a3a6797
Juan Jose Sapuyes <jsapuyesp@gmail.com>
2022-11-24 10:19:35 -0500

    Test section finished

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 
-
 export const CounterApp = ({ title, value }) => {
-  
-  const [ counter, setCounter] = useState(value);
-  
+  const [counter, setCounter] = useState(value);
+
   function addOne(event) {
     setCounter(counter + 1);
   }
   function minusOne(event) {
-    setCounter(counter - 1);  }
+    setCounter(counter - 1);
+  }
   function equalZero(event) {
     setCounter(value);
   }
@@ -21,18 +20,20 @@ export const CounterApp = ({ title, value }) => {
       <h2>{counter}</h2>
       <button onClick={addOne}> +1 </button>
       <button onClick={minusOne}> -1 </button>
-      <button onClick={equalZero}> Reset </button>
+      <button aria-label="btn-reset" onClick={equalZero}>
+        {" "}
+        Reset{" "}
+      </button>
     </>
-  )
-}
+  );
+};
 
 CounterApp.propTypes = {
   title: PropTypes.string,
-  value: PropTypes.number.isRequired
-}
+  value: PropTypes.number.isRequired,
+};
 
 CounterApp.defaultProps = {
-  title: 'Counter App',
+  title: "Counter App",
   value: 0,
-  
-}
+};
--- tests/CounterApp.test.jsx
+++ tests/CounterApp.test.jsx
@@ -1,26 +1,43 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { CounterApp } from "../src/CounterApp";
 
+describe("Test de <CounterApp/>", () => {
+  const initialValue = 10;
 
-
-describe('Test de <CounterApp/>', () => {
-  
-  const value = "100";
-
-  test('Debe hacer match con el snapshot', () => {
-    const { container } = render(
-      <CounterApp value={value}/>
-    );
+  test("Debe hacer match con el snapshot", () => {
+    const { container } = render(<CounterApp value={initialValue} />);
     expect(container).toMatchSnapshot();
   });
 
-  test('El valor inicial debe ser de 100', () => {
-    render(
-      <CounterApp value={value}/>
+  test("El valor inicial debe ser de 10", () => {
+    render(<CounterApp value={initialValue} />);
+    expect(screen.getByRole("heading", { level: 2 }).innerHTML).toContain(
+      initialValue.toString()
     );
-
-    expect(screen.getByRole('heading', {level:2}).innerHTML).toContain(value)
-
   });
 
+  test("Debe incrementar con el botón +1", () => {
+    render(<CounterApp value={initialValue} />);
+    fireEvent.click(screen.getByText("+1"));
+    expect(screen.getByText((initialValue + 1).toString())).toBeTruthy();
+  });
+
+  test("Debe reducir con el botón -1", () => {
+    render(<CounterApp value={initialValue} />);
+    fireEvent.click(screen.getByText("-1"));
+    expect(screen.getByText((initialValue - 1).toString())).toBeTruthy();
+  });
+
+  test("Debe reiniciar el valor", () => {
+    render(<CounterApp value={initialValue} />);
+    fireEvent.click(screen.getByText("-1"));
+    fireEvent.click(screen.getByText("-1"));
+    fireEvent.click(screen.getByText("-1"));
+
+    fireEvent.click(screen.getByRole("button", { name: "btn-reset" }));
+
+    expect(screen.getByRole("heading", { level: 2 }).innerHTML).toBe(
+      initialValue.toString()
+    );
+  });
 });
